80eba076eb95cc9e78ad85313d029452f1ece680
potrdev <ytbpotr@gmail.com>
2024-05-08 17:27:59 +0200

    new change 2

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 class Test:
-  def __init__(self):
-    self.name = "Patrik"
+  def __init__(self, name):
+    self.name = name
+  
+  def sayName(self):
+    print(self.name)
+    
+Patrik = Test("Patrik")
+Patrik.sayName()
